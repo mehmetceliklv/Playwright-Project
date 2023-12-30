0d112d1035c4f0f136beb24134fae417eb8170f1
Mehmet Celik <mehmet.celikk2708@gmail.com>
2023-12-30 19:55:40 +0200

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,31 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+
+const config = {
+  testDir: './tests',
+  retries :0,
+  
+  /* Maximum time one test can run for. */
+  timeout: 40 * 1000,
+  expect: {
+  
+    timeout: 5000
+  },
+  
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+
+    browserName : 'chromium',
+    headless : false,
+    screenshot : 'on',
+    trace : 'on',//off,on, retain-on-failure
+    
+    
+    
+  },
+
+
+};
+
+module.exports = config;
